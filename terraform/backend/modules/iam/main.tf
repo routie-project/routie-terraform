f7a3f8e3167e747d5e3e576b22e34047690428eb
goohong <goohong.chung@gmail.com>
2025-11-26 13:56:47 +0900

    refactor: CloudWatch 로그 그룹 구조 단순화
    - 여러 개의 로그 그룹을 환경별 단일 로그 그룹으로 통합
    
    - 로그 그룹 이름을 /routie/{environment} 형식으로 변경
    
    - IAM 모듈에 environment 변수 추가

--- terraform/backend/modules/iam/main.tf
+++ terraform/backend/modules/iam/main.tf
@@ -30,34 +30,8 @@ resource "aws_iam_instance_profile" "ec2_cw_profile" {
   role = aws_iam_role.ec2_cw_role.name
 }
 
-# CloudWatch Logs 그룹 생성
-resource "aws_cloudwatch_log_group" "syslog" {
-  name              = "/aws/ec2/routie/syslog"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_group" "nginx_access" {
-  name              = "/aws/ec2/routie/nginx/access"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_group" "nginx_error" {
-  name              = "/aws/ec2/routie/nginx/error"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_group" "routie_application" {
-  name              = "/aws/ec2/routie/application"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_group" "routie_exception" {
-  name              = "/aws/ec2/routie/exception"
-  retention_in_days = 7
-}
-
-resource "aws_cloudwatch_log_group" "routie_request" {
-  name              = "/aws/ec2/routie/request"
+resource "aws_cloudwatch_log_group" "routie_main" {
+  name              = "/routie/${var.environment}"
   retention_in_days = 7
 }
 
---
+++ terraform/backend/modules/iam/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., dev, prod)"
+}
+
